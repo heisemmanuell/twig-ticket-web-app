1cfb8e5aa3bcd0c589a4ed8ca4d2ad7e90a29a33
Chidindu Emmanuel <93309072+heisemmanuell@users.noreply.github.com>
2025-10-28 01:45:04 +0100

    added landing, login, signup, dashboard and ticket management pages

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: php -S localhost:3000 -t src
+php -S 0.0.0.0:$PORT -t src
--- src/index.php
+++ src/index.php
@@ -17,18 +17,33 @@ switch($path){
     case '/home':
         // Eg: passing a 'title' variable to the landing.twig template
         echo $twig->render('landing.twig',[
-            'title'=> "Landing Page - Twig Starter Template",
-            
+            'title'=> "Landing Page - Twig Ticket Web App",
+
+        ]);
+        break;
+    case '/auth/login':
+        echo $twig->render('auth/login.twig',[
+            'title'=> "Login - Twig Ticket Web App",
+        ]);
+        break;
+    case '/auth/signup':
+        echo $twig->render('auth/signup.twig',[
+            'title'=> "Sign Up - Twig Ticket Web App",
         ]);
         break;
     case '/dashboard':
         echo $twig->render('dashboard.twig',[
-            'title'=> "Dashboard - Twig Starter Template",
+            'title'=> "Dashboard - Twig Ticket Web App",
+        ]);
+        break;
+    case '/tickets':
+        echo $twig->render('tickets.twig',[
+            'title'=> "Tickets - Twig Ticket Web App",
         ]);
         break;
     default:
     echo $twig->render('404.twig',[
-        'title'=> "404 Not Found - Twig Starter Template",
+        'title'=> "404 Not Found - Twig Ticket Web App",
     ]);
 };
 ?>
